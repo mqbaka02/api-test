89859cb060fd07920376afb5672741b25a51b5e7
mqbaka\mqbaka <mqbaka@gmail.com>
2024-11-04 18:45:22 +0300

    beigining simple routing.

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <button id="fetcher">Fetch and log</button>
-    <script src="fetcher.js"></script>
-</body>
-</html>
+<?php
+$requested_url= $_SERVER['REQUEST_URI'];
+// echo $requested_url;
+$content= file_get_contents('views/main.php');
+echo $content;
+exit();
